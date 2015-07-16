eec5e8a47cf2a0848fd0c252cc501e0902e271ca
Alexander De Leon <me@alexdeleon.name>
2015-07-16 16:30:16 +0200

    ports and directories are bound to docker image therefore they are not configurable

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@ include_recipe "docker"
 docker_image node[:influxdb][:docker_image] do
 	tag node[:influxdb][:docker_image_tag]
 	action :pull
+	notifies :redeploy, 'docker_container[influxdb]', :immediately
 end
 
 # Create volume directory
@@ -20,27 +21,30 @@ end
 # Build the configuration
 template "#{node[:influxdb][:config_path]}/config.toml" do
 	source "config.toml.erb"
-	variables :config => node[:influxdb][:config]
+	variables ({
+			:config => node[:influxdb][:config],
+			:data_path => node[:influxdb][:container_data_path]
+		})
 	action :create
-	notifies :restart, "service[influxdb]", :delayed
+	notifies :restart, "docker_container[influxdb]", :delayed
 end
 
 # Run the docker container
+docker_port = as_list(node[:influxdb][:docker_ports])
 docker_container "influxdb" do
 	action :run
 	image "#{node[:influxdb][:docker_image]}:#{node[:influxdb][:docker_image_tag]}"
 	container_name node[:influxdb][:docker_container]
 	detach true
-	expose [ "#{node[:influxdb][:config][:admin][:port]}",
-					 "#{node[:influxdb][:config][:http][:port]}",
-					 '8090', '8099']
-					 # Ports [8090, 8099] only used for clustering purposes and should not be exposed
-	port   [ "#{node[:influxdb][:config][:admin][:port]}:#{node[:influxdb][:config][:admin][:port]}",
-				   "#{node[:influxdb][:config][:http][:port]}:#{node[:influxdb][:config][:http][:port]}" ]
+	port   docker_port
 	volume [ "#{node[:influxdb][:config_path]}/config.toml:#{node[:influxdb][:container_config_path]}/config.toml",
 				   "#{node[:influxdb][:data_path]}:#{node[:influxdb][:container_data_path]}"]
 end
 
-service "influxdb" do
-	action :nothing
+def as_list(value)
+  if value.is_a? String
+    value.split(',').map {|s| s.strip}
+  else
+    value
+  end
 end
